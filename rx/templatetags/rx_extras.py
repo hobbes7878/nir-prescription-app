090dce760d4f4648c6b4202fb52d07e43630264f
Jon <jon@jon-Satellite-U845W.(none)>
2014-01-28 21:52:17 -0600

    1-28-13 styles2

--- rx/templatetags/rx_extras.py
+++ rx/templatetags/rx_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 import re
+import math
 from rx.models import TopDrugGPs
 
 register = template.Library()
@@ -29,3 +30,16 @@ def monthly(value):
 def per_nir_gps(value):
 	all_nir_gps = 349
 	return value/all_nir_gps
+
+@register.filter
+def sigdig(value, digits = 3):
+	if value == 0:
+		return 0
+	else:
+	    order = int(math.floor(math.log10(math.fabs(value))))
+	    places = digits - order - 1
+	    if places > 0:
+	        fmtstr = "%%.%df" % (places)
+	    else:
+	        fmtstr = "%.0f"
+	    return fmtstr % (round(value, places))
